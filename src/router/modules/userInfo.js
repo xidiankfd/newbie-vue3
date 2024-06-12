92e26aa21ffef8d0284fe418dc4eb159e0495418
zhangyuge <398698424@qq.com>
2024-06-12 16:13:37 +0800

    删除前端框架部分示例

--- src/router/modules/userInfo.js
+++
@@ -1,9 +0,0 @@
-export default {
-  path: '/user-info',
-  name: 'UserInfo',
-  meta: {
-    title: 'menus.personalCenter',
-    icon: 'ep:user',
-  },
-  component: () => import('@/views/user-info/index.vue'),
-}
